781ac031e10faffdd25b51e98a92412263298c4e
Sreekanth-17 <148879990+Sreekanth-17@users.noreply.github.com>
2025-04-01 16:49:07 +0530

    Create Task_4.js

---
+++ Week_2/Task_4.js
@@ -0,0 +1,10 @@
+// Initialize an empty array
+let numbers = [];
+
+// Use a for loop to add numbers from 1 to 5
+for (let i = 1; i <= 5; i++) {
+    numbers.push(i);  // Add the current number to the array
+}
+
+// Output the array
+console.log(numbers);  // [1, 2, 3, 4, 5]
