fb7d617975e4de35721448e442cca4714b7c7b42
Sreekanth-17 <148879990+Sreekanth-17@users.noreply.github.com>
2025-04-01 16:49:57 +0530

    Create Task_5.js

---
+++ Week_2/Task_5.js
@@ -0,0 +1,8 @@
+// Function to calculate the square of a number
+function square(num) {
+    return num * num;  // Square the number
+}
+
+// Example usage
+let result = square(5);  // Calls the function with 5 as input
+console.log(result);  // Output: 25
