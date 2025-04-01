796f6ccc45f58c25887e0b262f8ba236bc0d5705
Sreekanth-17 <148879990+Sreekanth-17@users.noreply.github.com>
2025-04-01 16:48:19 +0530

    Create Task_3.js

---
+++ Week_2/Task_3.js
@@ -0,0 +1,8 @@
+// Declare a number n
+let n = 7;  // You can change this value to test with other numbers
+
+// Check if the number is even or odd
+let result = (n % 2 === 0) ? "Even" : "Odd";
+
+// Output the result
+console.log("The number " + n + " is " + result);
