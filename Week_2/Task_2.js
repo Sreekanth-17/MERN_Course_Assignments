383354be5f6547db1c00ff839a77a64ae9ed7e0c
Sreekanth-17 <148879990+Sreekanth-17@users.noreply.github.com>
2025-04-01 16:47:23 +0530

    Create Task_2.js

---
+++ Week_2/Task_2.js
@@ -0,0 +1,13 @@
+// Given values
+let x = 10;
+let y = 5;
+
+// Perform the operations
+let addition = x + y;         // Addition
+let multiplication = x * y;   // Multiplication
+let modulus = x % y;          // Modulus
+
+// Output the results
+console.log("Addition: " + addition);           // 10 + 5 = 15
+console.log("Multiplication: " + multiplication); // 10 * 5 = 50
+console.log("Modulus: " + modulus);             // 10 % 5 = 0
