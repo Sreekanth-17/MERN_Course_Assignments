3bc13dda17bb7bf3783ab7870105ca1df8448002
Sreekanth-17 <148879990+Sreekanth-17@users.noreply.github.com>
2025-04-01 16:46:24 +0530

    Create Task_1.js

---
+++ Week_2/Task_1.js
@@ -0,0 +1,8 @@
+// Declare a variable 'age' and assign it a value of 25
+let age = 25;
+
+// Declare a variable 'isAdult' and assign it true if age is 18 or more, otherwise false
+let isAdult = age >= 18;
+
+console.log("Age:", age); // Output: 25
+console.log("Is Adult:", isAdult); // Output: true
