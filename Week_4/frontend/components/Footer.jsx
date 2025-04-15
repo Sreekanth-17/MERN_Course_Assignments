55f434e314506f765acc2034fba815f8a92cc189
Sreekanth-17 <148879990+Sreekanth-17@users.noreply.github.com>
2025-04-16 03:12:55 +0530

    Create Footer.jsx

---
+++ Week_4/frontend/components/Footer.jsx
@@ -0,0 +1,15 @@
+// Footer.jsx
+import React from 'react';
+
+const Footer = () => (
+  <footer className="bg-blue-600 text-white text-center p-4 mt-8">
+    <p>Follow us:</p>
+    <div className="space-x-4">
+      <a href="#">Instagram</a>
+      <a href="#">Twitter</a>
+      <a href="#">Facebook</a>
+    </div>
+  </footer>
+);
+
+export default Footer;
